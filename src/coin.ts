6ea5eff8610e30b9e7a57a14cff898a4f7ea218e
lexie-ai <kevinxu01291@gmail.com>
2022-11-22 05:26:32 -0800

    initial commit

---
+++ src/coin.ts
@@ -0,0 +1,10 @@
+// Represent a specific value of a coin
+class Coin {
+  private value: number;
+
+  constructor(value: number) {
+    this.value = value;
+  }
+}
+
+export default Coin;
---
+++ tests/index.spec.ts
@@ -0,0 +1,7 @@
+//********************************************************************************
+// Part 2 : Tests
+//
+// * Describe your process to tests some of the functions and properties above
+// * It can either be code, or just commented explanations on the testing procedure
+//   (what, how, ..)
+//********************************************************************************
